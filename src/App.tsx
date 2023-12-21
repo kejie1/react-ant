4ab8770d41ec3a840d29a5cf13f9992127b05f7f
ChuandongHuang <chuandong_huang@human-horizons.com>
2023-12-21 13:44:34 +0800

    fix：废弃文件删除

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/*
+ * @Author: ChuandongHuang chuandong_huang@human-horizons.com
+ * @Date: 2023-12-20 16:29:35
+ * @LastEditors: ChuandongHuang chuandong_huang@human-horizons.com
+ * @LastEditTime: 2023-12-20 17:09:05
+ * @Description: 
+ */
+import {Button} from 'antd'
+const App = ()=>{
+  return(
+    <div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button type='primary'>antd</Button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,21 @@
+/*
+ * @Author: ChuandongHuang chuandong_huang@human-horizons.com
+ * @Date: 2023-12-20 16:29:35
+ * @LastEditors: ChuandongHuang chuandong_huang@human-horizons.com
+ * @LastEditTime: 2023-12-20 17:14:47
+ * @Description: 
+ */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.scss';
+import router from './router'
+import { RouterProvider } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
