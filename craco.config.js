bf4f2ec9a16b58e1e4b4ca563c9c862650460d20
ChuandongHuang <chuandong_huang@human-horizons.com>
2023-12-22 18:02:14 +0800

    fix：临时提交

--- craco.config.js
+++ craco.config.js
@@ -2,15 +2,17 @@
  * @Author: ChuandongHuang chuandong_huang@human-horizons.com
  * @Date: 2023-12-20 17:30:08
  * @LastEditors: ChuandongHuang chuandong_huang@human-horizons.com
- * @LastEditTime: 2023-12-20 17:31:37
+ * @LastEditTime: 2023-12-22 17:37:15
  * @Description: 
  */
 const path = require('path')
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
 
 module.exports = {
     webpack:{
         alias:{
             '@':path.resolve(__dirname,'src')
-        }
+        },
+        plugins: [new NodePolyfillPlugin()]
     }
 }
