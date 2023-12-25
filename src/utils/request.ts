aa312f9ddf724f522e2952cc1c4adb349d54b56a
ChuandongHuang <chuandong_huang@human-horizons.com>
2023-12-25 13:53:00 +0800

    feat：无无状态处理

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -2,16 +2,15 @@
  * @Author: ChuandongHuang chuandong_huang@human-horizons.com
  * @Date: 2023-12-21 13:35:04
  * @LastEditors: ChuandongHuang chuandong_huang@human-horizons.com
- * @LastEditTime: 2023-12-21 16:51:15
+ * @LastEditTime: 2023-12-25 10:54:32
  * @Description: axios封装
  */
 // axios封装
 import axios from "axios";
 import { AxiosResponse } from "axios";
-import { getToken } from "@/utils/index";
-import { useNavigate } from "react-router";
+import { getToken, removeToken } from "@/utils/index";
 import { message } from "antd";
-
+import router from "@/router";
 const request = axios.create({
   baseURL: "http://geek.itheima.net/v1_0",
   timeout: 1000 * 5,
@@ -41,6 +40,12 @@ request.interceptors.response.use(
     return response.data;
   },
   (error) => {
+    if(error.response.status === 401){
+      removeToken()
+      message.warning('登录状态失效，请重新登录')
+      router.navigate('/login')
+      window.location.reload()
+    }
     return Promise.reject(error.response);
   }
 );
